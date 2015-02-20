9e7e380f8ae42458e8fd9d9c32761cd67f132e02
Yoeri Otten <yoeori@live.nl>
2015-02-20 23:53:01 +0100

    Rails autoloading not working, temp fix

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,11 +1,15 @@
+Dir[Rails.root + 'app/models/**/*.rb'].each do |path|
+  require path
+end
+
 class StoresController < ApplicationController
 
   def all #/v1/stores
-    render json: Store.all, root: false
+    render json: ::Store.all, root: false
   end
 
   def store #/v1/store/:uuid
-  	render json: Store.where(uuid: params[:uuid]).first, root: false, :serializer => StorefullSerializer
+  	render json: ::Store.where(uuid: params[:uuid]).first, root: false, :serializer => StorefullSerializer
   end
 
 end
